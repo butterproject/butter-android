54655977d41d178e18d328f49cc8cd8bb8cbf040
sv244 <sv244@popcorntime.io>
2014-11-06 10:44:16 +0100

    Add global PopcornApplication class

---
+++ app/src/main/java/pct/droid/PopcornApplication.java
@@ -0,0 +1,17 @@
+package pct.droid;
+
+import android.app.Application;
+
+public class PopcornApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+    
+}
