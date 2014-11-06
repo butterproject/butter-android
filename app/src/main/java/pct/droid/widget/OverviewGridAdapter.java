5739a1e486f7e175749ec73d079b8e18752b7ab1
sv244 <sv244@popcorntime.io>
2014-11-06 20:02:41 +0100

    Add some padding in overview

--- app/src/main/java/pct/droid/widget/OverviewGridAdapter.java
+++ app/src/main/java/pct/droid/widget/OverviewGridAdapter.java
@@ -20,6 +20,7 @@ import pct.droid.R;
 import pct.droid.providers.media.MediaProvider;
 import pct.droid.providers.media.YTSProvider;
 import pct.droid.utils.LogUtils;
+import pct.droid.utils.PixelUtils;
 
 
 public class OverviewGridAdapter extends RecyclerView.Adapter<OverviewGridAdapter.ViewHolder> {
@@ -29,12 +30,15 @@ public class OverviewGridAdapter extends RecyclerView.Adapter<OverviewGridAdapte
     OverviewGridAdapter.OnItemClickListener mItemClickListener;
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        View itemView;
         @InjectView(R.id.coverImage)
         ImageView coverImage;
 
         public ViewHolder(View itemView) {
             super(itemView);
             ButterKnife.inject(this, itemView);
+            this.itemView = itemView;
             itemView.setOnClickListener(this);
         }
 
@@ -52,9 +56,9 @@ public class OverviewGridAdapter extends RecyclerView.Adapter<OverviewGridAdapte
         public void onItemClick(View v, MediaProvider.Video item, int position);
     }
 
-    public OverviewGridAdapter(Activity activity, ArrayList<MediaProvider.Video> items) {
+    public OverviewGridAdapter(Activity activity, ArrayList<MediaProvider.Video> items, Integer columns) {
         mItems = items;
-        mItemWidth = (activity.getWindow().getDecorView().getWidth() / 2);
+        mItemWidth = (activity.getWindow().getDecorView().getWidth() / columns);
         mItemHeight = (int) (1.5 * (double) mItemWidth);
     }
 
@@ -68,6 +72,15 @@ public class OverviewGridAdapter extends RecyclerView.Adapter<OverviewGridAdapte
     @Override
     public void onBindViewHolder(OverviewGridAdapter.ViewHolder viewHolder, int position) {
         final MediaProvider.Video item = getItem(position);
+
+        int padding = PixelUtils.getPixelsFromDp(viewHolder.itemView.getContext(), 2);
+        int double_padding = padding * 2;
+        int top_padding = (position == 0 || position == 1) ? padding * 2 : padding;
+        if(position % 2 == 0) {
+            viewHolder.itemView.setPadding(double_padding, top_padding, padding, padding);
+        } else {
+            viewHolder.itemView.setPadding(padding, top_padding, double_padding, padding);
+        }
 
         Picasso.with(viewHolder.coverImage.getContext()).load(item.image)
                 .placeholder(R.drawable.transparant)
