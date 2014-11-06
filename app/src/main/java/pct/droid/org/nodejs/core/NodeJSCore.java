2f14af92aeb5582ebcc585e079c5fec70dc458f6
sv244 <sv244@popcorntime.io>
2014-11-06 11:50:16 +0100

    Add logs on JNI loading

--- app/src/main/java/pct/droid/org/nodejs/core/NodeJSCore.java
+++ app/src/main/java/pct/droid/org/nodejs/core/NodeJSCore.java
@@ -1,15 +1,16 @@
 package pct.droid.org.nodejs.core;
 
-/**
- * Created by wally on 06/11/14.
- */
+import pct.droid.utils.LogUtils;
+
 public class NodeJSCore {
     private static final String TAG = "nodejs-core";
 
     public static native void run(String mainJS);
 
     static {
+        LogUtils.d(TAG, "Loading libnodeJNI");
         System.loadLibrary("nodeJNI");
+        LogUtils.d(TAG, "libnodeJNI Loaded");
     }
 
 }
