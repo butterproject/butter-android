d917e6f75baccaf1d06e27aaa7150892b6b0db61
sv244 <sv244@live.nl>
2014-11-19 00:10:44 +0100

    Subtitle classes done

--- app/src/main/java/pct/droid/providers/subs/SubsProvider.java
+++ app/src/main/java/pct/droid/providers/subs/SubsProvider.java
@@ -1,9 +1,23 @@
 package pct.droid.providers.subs;
 
+import com.squareup.okhttp.Call;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import pct.droid.providers.BaseProvider;
 
 public abstract class SubsProvider extends BaseProvider {
 
-    public abstract void getList();
+    public abstract Call getList(String[] imdbIds, Callback callback);
+
+    public Call getList(String imdbId, Callback callback) {
+        return getList(new String[] { imdbId }, callback);
+    }
+
+    public interface Callback {
+        public void onSuccess(HashMap<String, HashMap<String, String>> items);
+        public void onFailure(Exception e);
+    }
 
 }
