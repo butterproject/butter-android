fef15bdfdefef48d85f4918e2ed9391c2ce1886e
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-11-29 00:38:53 +0100

    Fix resume time

--- mobile/src/main/java/pct/droid/utils/PrefUtils.java
+++ mobile/src/main/java/pct/droid/utils/PrefUtils.java
@@ -21,12 +21,28 @@ public class PrefUtils {
         return getPrefs(context).getString(key, defaultValue);
     }
 
-    public static void save(Context context, String key, Boolean value) {
+    public static void save(Context context, String key, boolean value) {
         getPrefs(context).edit().putBoolean(key, value).apply();
     }
 
-    public static Boolean get(Context context, String key, Boolean defaultValue) {
+    public static Boolean get(Context context, String key, boolean defaultValue) {
         return getPrefs(context).getBoolean(key, defaultValue);
+    }
+
+    public static void save(Context context, String key, long value) {
+        getPrefs(context).edit().putLong(key, value).apply();
+    }
+
+    public static long get(Context context, String key, long defaultValue) {
+        return getPrefs(context).getLong(key, defaultValue);
+    }
+
+    public static void save(Context context, String key, int value) {
+        getPrefs(context).edit().putInt(key, value).apply();
+    }
+
+    public static int get(Context context, String key, int defaultValue) {
+        return getPrefs(context).getInt(key, defaultValue);
     }
 
     public static Boolean contains(Context context, String key) {
