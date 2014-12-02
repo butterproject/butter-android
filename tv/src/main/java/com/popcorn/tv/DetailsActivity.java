e25b489799e9139a6d00df1c58aa4cb6cd147525
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-12-02 01:47:11 +0100

    Created the androidTV module

---
+++ tv/src/main/java/com/popcorn/tv/DetailsActivity.java
@@ -0,0 +1,15 @@
+package com.popcorn.tv;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailsActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
