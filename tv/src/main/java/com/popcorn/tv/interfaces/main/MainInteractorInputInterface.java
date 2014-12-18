c689fa4c151f9a4e846f635107ac037c296d7555
Pedro <pepibumur@gmail.com>
2014-12-18 03:59:08 +0100

    Started the interaction with the API to fill the layout with real data

--- tv/src/main/java/com/popcorn/tv/interfaces/main/MainInteractorInputInterface.java
+++ tv/src/main/java/com/popcorn/tv/interfaces/main/MainInteractorInputInterface.java
@@ -1,6 +1,15 @@
 package com.popcorn.tv.interfaces.main;
 
+import android.content.Context;
+import android.support.v17.leanback.widget.ArrayObjectAdapter;
+import android.support.v17.leanback.widget.HeaderItem;
+import android.support.v17.leanback.widget.Row;
+
 public interface MainInteractorInputInterface
 {
-
+    public int getNumberOfSections(Context context);
+    public HeaderItem getSectionHeaderAtIndex(int index, Context context);
+    public ArrayObjectAdapter getSectionAdapterAtIndex(int index, Context context);
+    public int getRightItemsNextTo(Object item, Row row);
+    public void getMore(Row row);
 }
