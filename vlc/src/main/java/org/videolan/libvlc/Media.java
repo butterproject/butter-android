ac6b6925607e02d858b7908847057fd7fd2238ed
sv244 <sv244@live.nl>
2014-12-23 21:06:05 +0100

    Auto-optimise code in the dep modules

--- vlc/src/main/java/org/videolan/libvlc/Media.java
+++ vlc/src/main/java/org/videolan/libvlc/Media.java
@@ -20,13 +20,13 @@
 
 package org.videolan.libvlc;
 
+import android.graphics.Bitmap;
+import android.util.Log;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Locale;
-
-import android.graphics.Bitmap;
-import android.util.Log;
 
 public class Media implements Comparable<Media> {
     public final static String TAG = "VLC/LibVLC/Media";
@@ -43,14 +43,14 @@ public class Media implements Comparable<Media> {
                 ".m4v", ".mkv", ".mov", ".mp2", ".mp2v", ".mp4", ".mp4v", ".mpe", ".mpeg",
                 ".mpeg1", ".mpeg2", ".mpeg4", ".mpg", ".mpv2", ".mts", ".mtv", ".mxf", ".mxg",
                 ".nsv", ".nut", ".nuv", ".ogm", ".ogv", ".ogx", ".ps", ".rec", ".rm", ".rmvb",
-                ".tod", ".ts", ".tts", ".vob", ".vro", ".webm", ".wm", ".wmv", ".wtv", ".xesc" };
+                ".tod", ".ts", ".tts", ".vob", ".vro", ".webm", ".wm", ".wmv", ".wtv", ".xesc"};
 
         String[] audio_extensions = {
                 ".3ga", ".a52", ".aac", ".ac3", ".adt", ".adts", ".aif", ".aifc", ".aiff", ".amr",
                 ".aob", ".ape", ".awb", ".caf", ".dts", ".flac", ".it", ".m4a", ".m4b", ".m4p",
                 ".mid", ".mka", ".mlp", ".mod", ".mpa", ".mp1", ".mp2", ".mp3", ".mpc", ".mpga",
                 ".oga", ".ogg", ".oma", ".opus", ".ra", ".ram", ".rmi", ".s3m", ".spx", ".tta",
-                ".voc", ".vqf", ".w64", ".wav", ".wma", ".wv", ".xa", ".xm" };
+                ".voc", ".vqf", ".w64", ".wav", ".wma", ".wv", ".xa", ".xm"};
 
         String[] folder_blacklist = {
                 "/alarms",
@@ -62,7 +62,7 @@ public class Media implements Comparable<Media> {
                 "/media/audio/alarms",
                 "/media/audio/notifications",
                 "/media/audio/ringtones",
-                "/Android/data/" };
+                "/Android/data/"};
 
         VIDEO_EXTENSIONS = new HashSet<String>();
         for (String item : video_extensions)
@@ -74,11 +74,11 @@ public class Media implements Comparable<Media> {
         StringBuilder sb = new StringBuilder(115);
         sb.append(".+(\\.)((?i)(");
         sb.append(video_extensions[0].substring(1));
-        for(int i = 1; i < video_extensions.length; i++) {
+        for (int i = 1; i < video_extensions.length; i++) {
             sb.append('|');
             sb.append(video_extensions[i].substring(1));
         }
-        for(int i = 0; i < audio_extensions.length; i++) {
+        for (int i = 0; i < audio_extensions.length; i++) {
             sb.append('|');
             sb.append(audio_extensions[i].substring(1));
         }
@@ -94,7 +94,9 @@ public class Media implements Comparable<Media> {
     public final static int TYPE_AUDIO = 1;
     public final static int TYPE_GROUP = 2;
 
-    /** Metadata from libvlc_media */
+    /**
+     * Metadata from libvlc_media
+     */
     protected String mTitle;
     private String mArtist;
     private String mGenre;
@@ -125,11 +127,12 @@ public class Media implements Comparable<Media> {
 
     /**
      * Create a new Media
+     *
      * @param libVLC A pointer to the libVLC instance. Should not be NULL
-     * @param URI The URI of the media.
+     * @param URI    The URI of the media.
      */
     public Media(LibVLC libVLC, String URI) {
-        if(libVLC == null)
+        if (libVLC == null)
             throw new NullPointerException("libVLC was null");
 
         mLocation = URI;
@@ -149,7 +152,7 @@ public class Media implements Comparable<Media> {
                 mType = TYPE_VIDEO;
                 mWidth = track.Width;
                 mHeight = track.Height;
-            } else if (mType == TYPE_ALL && track.Type == TrackInfo.TYPE_AUDIO){
+            } else if (mType == TYPE_ALL && track.Type == TrackInfo.TYPE_AUDIO) {
                 mType = TYPE_AUDIO;
             } else if (track.Type == TrackInfo.TYPE_META) {
                 mLength = track.Length;
@@ -170,7 +173,7 @@ public class Media implements Comparable<Media> {
             int dotIndex = mLocation.lastIndexOf(".");
             if (dotIndex != -1) {
                 String fileExt = mLocation.substring(dotIndex).toLowerCase(Locale.ENGLISH);
-                if( Media.VIDEO_EXTENSIONS.contains(fileExt) ) {
+                if (Media.VIDEO_EXTENSIONS.contains(fileExt)) {
                     mType = TYPE_VIDEO;
                 } else if (Media.AUDIO_EXTENSIONS.contains(fileExt)) {
                     mType = TYPE_AUDIO;
@@ -180,8 +183,8 @@ public class Media implements Comparable<Media> {
     }
 
     public Media(String location, long time, long length, int type,
-            Bitmap picture, String title, String artist, String genre, String album,
-            int width, int height, String artworkURL, int audio, int spu) {
+                 Bitmap picture, String title, String artist, String genre, String album,
+                 int width, int height, String artworkURL, int audio, int spu) {
         mLocation = location;
         mFilename = null;
         mTime = time;
@@ -200,37 +203,39 @@ public class Media implements Comparable<Media> {
         mArtworkURL = artworkURL;
     }
 
-    private enum UnknownStringType { Artist , Genre, Album };
+    private enum UnknownStringType {Artist, Genre, Album}
+
+    ;
+
     /**
      * Uses introspection to read VLC l10n databases, so that we can sever the
      * hard-coded dependency gracefully for 3rd party libvlc apps while still
      * maintaining good l10n in VLC for Android.
      *
-     * @see org.videolan.vlc.util.Util#getValue(String, int)
-     *
      * @param string The default string
-     * @param type Alias for R.string.xxx
+     * @param type   Alias for R.string.xxx
      * @return The default string if not empty or string from introspection
+     * @see org.videolan.vlc.util.Util#getValue(String, int)
      */
     private static String getValueWrapper(String string, UnknownStringType type) {
-        if(string != null && string.length() > 0) return string;
+        if (string != null && string.length() > 0) return string;
 
         try {
             Class<?> stringClass = Class.forName("org.videolan.vlc.R$string");
             Class<?> utilClass = Class.forName("org.videolan.vlc.Util");
 
             Integer value;
-            switch(type) {
-            case Album:
-                value = (Integer)stringClass.getField("unknown_album").get(null);
-                break;
-            case Genre:
-                value = (Integer)stringClass.getField("unknown_genre").get(null);
-                break;
-            case Artist:
-            default:
-                value = (Integer)stringClass.getField("unknown_artist").get(null);
-                break;
+            switch (type) {
+                case Album:
+                    value = (Integer) stringClass.getField("unknown_album").get(null);
+                    break;
+                case Genre:
+                    value = (Integer) stringClass.getField("unknown_genre").get(null);
+                    break;
+                case Artist:
+                default:
+                    value = (Integer) stringClass.getField("unknown_artist").get(null);
+                    break;
             }
 
             Method getValueMethod = utilClass.getDeclaredMethod("getValue", String.class, Integer.TYPE);
@@ -246,14 +251,14 @@ public class Media implements Comparable<Media> {
 
         // VLC for Android translations not available (custom app perhaps)
         // Use hardcoded English phrases.
-        switch(type) {
-        case Album:
-            return "Unknown Album";
-        case Genre:
-            return "Unknown Genre";
-        case Artist:
-        default:
-            return "Unknown Artist";
+        switch (type) {
+            case Album:
+                return "Unknown Album";
+            case Genre:
+                return "Unknown Genre";
+            case Artist:
+            default:
+                return "Unknown Artist";
         }
     }
 
@@ -324,7 +329,7 @@ public class Media implements Comparable<Media> {
     /**
      * Returns the raw picture object. Likely to be NULL in VLC for Android
      * due to lazy-loading.
-     *
+     * <p/>
      * Use {@link org.videolan.vlc.util.Bitmap#getPictureFromCache(Media)} instead.
      *
      * @return The raw picture or NULL
@@ -335,7 +340,7 @@ public class Media implements Comparable<Media> {
 
     /**
      * Sets the raw picture object.
-     *
+     * <p/>
      * In VLC for Android, use {@link org.videolan.vlc.MediaDatabase#setPicture(Media, Bitmap)} instead.
      *
      * @param p
@@ -375,9 +380,9 @@ public class Media implements Comparable<Media> {
     }
 
     public String getGenre() {
-        if(getValueWrapper(null, UnknownStringType.Genre).equals(mGenre))
+        if (getValueWrapper(null, UnknownStringType.Genre).equals(mGenre))
             return mGenre;
-        else if( mGenre.length() > 1)/* Make genres case insensitive via normalisation */
+        else if (mGenre.length() > 1)/* Make genres case insensitive via normalisation */
             return Character.toUpperCase(mGenre.charAt(0)) + mGenre.substring(1).toLowerCase(Locale.getDefault());
         else
             return mGenre;
