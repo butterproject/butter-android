b6fbd5be4589403ac43d716580e61f0525528127
sv244 <sv244@live.nl>
2014-12-27 23:14:32 +0100

    Add empty SettingsActivity

---
+++ mobile/src/main/java/pct/droid/activities/SettingsActivity.java
@@ -0,0 +1,13 @@
+package pct.droid.activities;
+
+import android.os.Bundle;
+
+import pct.droid.R;
+
+public class SettingsActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState, R.layout.activity_settings);
+    }
+}
