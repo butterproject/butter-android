a8ae77478f9c0435c62f562420da40a422c31bce
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-12-28 03:01:57 +0100

    Add settings keys

--- mobile/src/main/java/pct/droid/activities/SettingsActivity.java
+++ mobile/src/main/java/pct/droid/activities/SettingsActivity.java
@@ -6,6 +6,12 @@ import pct.droid.R;
 
 public class SettingsActivity extends BaseActivity {
 
+    public static final String SUBTITLE_COLOR = "subtitle_color";
+    public static final String SUBTITLE_SIZE = "subtitle_size";
+    public static final String SUBTITLE_DEFAULT = "subtitle_default_language";
+    public static final String STORAGE_LOCATION = "storage_location";
+    public static final String HW_ACCELERATION = "hw_acceleration";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState, R.layout.activity_settings);
