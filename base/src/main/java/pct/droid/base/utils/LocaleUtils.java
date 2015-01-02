ab886246b4694e5a6cdd3cf31c7df465d291d397
Some Popcorn Time Contributor <pochoclin@popcorntime>
2015-01-02 01:23:04 +0100

    Make LocaleUtils

---
+++ base/src/main/java/pct/droid/base/utils/LocaleUtils.java
@@ -0,0 +1,40 @@
+package pct.droid.base.utils;
+
+import android.content.res.Configuration;
+
+import java.util.Locale;
+
+import pct.droid.base.PopcornApplication;
+import pct.droid.base.preferences.Prefs;
+
+public class LocaleUtils {
+
+    public static String getCurrent() {
+        return getLanguageCode(Locale.getDefault());
+    }
+
+    public static void setCurrent(Locale locale) {
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+
+        PopcornApplication.getAppContext().getResources().updateConfiguration(config, PopcornApplication.getAppContext().getResources().getDisplayMetrics());
+    }
+
+    public static String getLanguageCode(Locale locale) {
+        String languageCode = locale.getLanguage();
+        if(!locale.getCountry().isEmpty()) {
+            languageCode += "-" + locale.getCountry();
+        }
+        return languageCode;
+    }
+
+    public static Locale toLocale(String languageCode) {
+        String[] language = languageCode.split("-");
+        if(language.length > 1) {
+            return new Locale(language[0], language[1]);
+        }
+        return new Locale(language[0]);
+    }
+
+}
