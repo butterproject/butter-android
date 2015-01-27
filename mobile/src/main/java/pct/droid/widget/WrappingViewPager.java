352d2628836ae4315919c8e7315e7b7164e1509d
Some Popcorn Time Contributor <pochoclin@popcorntime>
2015-01-27 10:43:42 +0100

    - Add ViewPager height animation - Fix Toolbar fade out position

--- mobile/src/main/java/pct/droid/widget/WrappingViewPager.java
+++ mobile/src/main/java/pct/droid/widget/WrappingViewPager.java
@@ -5,8 +5,12 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 
 public class WrappingViewPager extends ViewPager {
+
+    private Boolean mAnimStarted = false;
  
     public WrappingViewPager(Context context) {
         super(context);
@@ -27,7 +31,55 @@ public class WrappingViewPager extends ViewPager {
             height = child.getMeasuredHeight();
         }
 
-        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+        // Not the best place to put this animation, but it works pretty good.
+        int newHeight = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+        if(getLayoutParams().height != 0) {
+            if (heightMeasureSpec != newHeight && !mAnimStarted) {
+                final int targetHeight = height;
+                final int currentHeight = getLayoutParams().height;
+                final int heightChange = targetHeight - currentHeight;
+
+                Animation a = new Animation() {
+                    @Override
+                    protected void applyTransformation(float interpolatedTime, Transformation t) {
+                        if (interpolatedTime >= 1) {
+                            getLayoutParams().height = targetHeight;
+                        } else {
+                            int stepHeight = (int) (heightChange * interpolatedTime);
+                            getLayoutParams().height = currentHeight + stepHeight;
+                        }
+                        requestLayout();
+                    }
+
+                    @Override
+                    public boolean willChangeBounds() {
+                        return true;
+                    }
+                };
+
+                a.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        mAnimStarted = true;
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        mAnimStarted = false;
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                    }
+                });
+
+                a.setDuration(300);
+                startAnimation(a);
+                mAnimStarted = true;
+            }
+        } else {
+            heightMeasureSpec = newHeight;
+        }
  
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
