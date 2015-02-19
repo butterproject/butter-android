1782a0cf4473a80e70ca1772b38a11d12a51380a
sv244 <sv244@popcorntime.io>
2015-02-20 00:44:59 +0100

    Replace NanoHTTPD by AndroidAsync for the CastingServer

--- base/src/main/java/pct/droid/base/casting/server/WebServerPluginInfo.java
+++
@@ -1,9 +0,0 @@
-package pct.droid.base.casting.server;
-
-public interface WebServerPluginInfo {
-    String[] getMimeTypes();
-
-    String[] getIndexFilesForMimeType(String mime);
-
-    WebServerPlugin getWebServerPlugin(String mimeType);
-}
