7a8ea1a62030d9781f11363025de6024fe3cb61a
Some Popcorn Time Contributor <pochoclin@popcorntime>
2015-03-04 02:11:30 +0100

    Update LibVLC to 1.1.0 Android base

--- vlc/src/main/java/org/videolan/vlc/util/WeakHandler.java
+++ vlc/src/main/java/org/videolan/vlc/util/WeakHandler.java
@@ -20,9 +20,9 @@
 
 package org.videolan.vlc.util;
 
-import android.os.Handler;
-
 import java.lang.ref.WeakReference;
+
+import android.os.Handler;
 
 public abstract class WeakHandler<T> extends Handler {
     private WeakReference<T> mOwner;
