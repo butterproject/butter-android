6043466fa6c418461a6418afa9258562dc8af37f
seifane <seifane53@gmail.com>
2015-07-04 20:54:32 +0000

    Genres tab now switch to the Popular tab when new genre is selected

--- mobile/src/main/java/pct/droid/fragments/MediaContainerFragment.java
+++ mobile/src/main/java/pct/droid/fragments/MediaContainerFragment.java
@@ -55,7 +55,7 @@ public class MediaContainerFragment extends Fragment {
 
         MediaProvider provider = getArguments().getParcelable(EXTRA_PROVIDER);
 
-        mAdapter = new MediaPagerAdapter(provider, getChildFragmentManager(), provider.getNavigation());
+        mAdapter = new MediaPagerAdapter(provider, getChildFragmentManager(), provider.getNavigation(), mViewPager);
         mViewPager.setAdapter(mAdapter);
         mViewPager.setCurrentItem(provider.getDefaultNavigationIndex());
         mTabs.setViewPager(mViewPager);
