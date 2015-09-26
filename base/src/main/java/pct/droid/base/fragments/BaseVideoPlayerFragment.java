5a0cb77e383302e8126a89d215764e53c111bf04
Some Popcorn Time Contributor <pochoclin@popcorntime>
2015-09-26 23:40:38 +0200

    Refactor things
    Former-commit-id: d46f4a9d0143c911203fc111242fc4eb71932255

--- base/src/main/java/pct/droid/base/fragments/BaseVideoPlayerFragment.java
+++ base/src/main/java/pct/droid/base/fragments/BaseVideoPlayerFragment.java
@@ -19,7 +19,6 @@ package pct.droid.base.fragments;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Canvas;
 import android.graphics.PorterDuff;
@@ -39,7 +38,6 @@ import android.view.MenuItem;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.connectsdk.device.ConnectableDevice;
