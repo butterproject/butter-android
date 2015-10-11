1d8f0ffb7ee393a166e31a281b8b4004ce2d8773
Some Popcorn Time Contributor <pochoclin@popcorntime>
2015-10-12 00:53:03 +0200

    Move wrong abi dialog to base activity
    Former-commit-id: b131036c391bc32a2cc1ee539a0ef6a8c468ff7f

--- tv/src/main/java/pct/droid/tv/activities/base/PTVBaseActivity.java
+++ tv/src/main/java/pct/droid/tv/activities/base/PTVBaseActivity.java
@@ -1,10 +1,39 @@
 package pct.droid.tv.activities.base;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
 import com.github.sv244.torrentstream.Torrent;
 
+import pct.droid.base.updater.PopcornUpdater;
+import pct.droid.base.utils.VersionUtils;
 import pct.droid.tv.activities.PTVSearchActivity;
 
 public abstract class PTVBaseActivity extends PTVTorrentBaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState, int layoutId) {
+		super.onCreate(savedInstanceState, layoutId);
+
+		if(!VersionUtils.isUsingCorrectBuild()) {
+			new AlertDialog.Builder(this)
+					.setMessage(pct.droid.base.R.string.wrong_abi)
+					.setCancelable(false)
+					.show();
+
+			PopcornUpdater.getInstance(this, new PopcornUpdater.Listener() {
+				@Override
+				public void updateAvailable(String updateFile) {
+					Intent installIntent = new Intent(Intent.ACTION_VIEW);
+					installIntent.setDataAndType(Uri.parse("file://" + getFilesDir().getAbsolutePath() + "/" + updateFile), PopcornUpdater.ANDROID_PACKAGE);
+					installIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+					startActivity(installIntent);
+				}
+			}).checkUpdatesManually();
+		}
+	}
 
 	@Override
 	public boolean onSearchRequested() {
