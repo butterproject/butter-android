4ac6332d35b82a2f7e5b49a61cc358e08bcb29bb
Some Popcorn Time Contributor <pochoclin@popcorntime>
2015-10-14 10:35:12 +0200

    Move debug ic_launcher back to mobile
    Former-commit-id: ce5e5f39c91e08839aeb21ce60380fa542476980

--- base/src/main/java/pct/droid/base/utils/VersionUtils.java
+++ base/src/main/java/pct/droid/base/utils/VersionUtils.java
@@ -32,6 +32,10 @@ import timber.log.Timber;
 
 public class VersionUtils {
 
+    public static boolean isMarshmallow() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+    }
+
     public static boolean isLollipop() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
     }
