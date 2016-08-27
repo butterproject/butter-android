18baa1c61dc8a29cddb6ee8f5b06e19cf120e168
skehmatics <skehmatics@gmail.com>
2016-08-27 07:03:13 -0700

    Moved Automatic port and Listening port preference into new Networking section
    Keeps the Torrent section cleaner and helps deter from tinkering

--- base/src/main/java/butter/droid/base/content/preferences/PreferencesHandler.java
+++ base/src/main/java/butter/droid/base/content/preferences/PreferencesHandler.java
@@ -472,6 +472,59 @@ public interface PreferencesHandler {
                     })
                     .build());
 
+
+            if(!isTV)
+            prefItems.add(PrefItem.newBuilder(context)
+                    .setIconResource(R.drawable.ic_prefs_storage_location)
+                    .setTitleResource(R.string.storage_location)
+                    .setPreferenceKey(Prefs.STORAGE_LOCATION)
+                    .hasNext(true)
+                    .setDefaultValue(StorageUtils.getIdealCacheDirectory(context))
+                    .setOnClickListener(new PrefItem.OnClickListener() {
+                        @Override
+                        public void onClick(final PrefItem item) {
+                            handler.openListSelection(item.getTitle(), null, SelectionMode.DIRECTORY, item.getValue(), 0, 0, new OnSelectionListener() {
+                                @Override
+                                public void onSelection(int position, Object value) {
+                                    if(value != null) {
+                                        item.saveValue(value);
+                                    } else {
+                                        item.clearValue();
+                                    }
+                                }
+                            });
+                        }
+                    })
+                    .setSubtitleGenerator(new PrefItem.SubtitleGenerator() {
+                        @Override
+                        public String get(PrefItem item) {
+                            return item.getValue().toString();
+                        }
+                    })
+                    .build());
+
+            prefItems.add(PrefItem.newBuilder(context)
+                    .setIconResource(R.drawable.ic_prefs_remove_cache)
+                    .setTitleResource(R.string.remove_cache)
+                    .setPreferenceKey(Prefs.REMOVE_CACHE)
+                    .setDefaultValue(true)
+                    .setOnClickListener(new PrefItem.OnClickListener() {
+                        @Override
+                        public void onClick(final PrefItem item) {
+                            item.saveValue(!(boolean) item.getValue());
+                        }
+                    })
+                    .setSubtitleGenerator(new PrefItem.SubtitleGenerator() {
+                        @Override
+                        public String get(PrefItem item) {
+                            boolean enabled = (boolean) item.getValue();
+                            return enabled ? context.getString(R.string.enabled) : context.getString(R.string.disabled);
+                        }
+                    })
+                    .build());
+
+            prefItems.add(PrefItem.newBuilder(context).setTitleResource(R.string.networking).build());
+
             prefItems.add(PrefItem.newBuilder(context)
                     .setIconResource(R.drawable.ic_prefs_random)
                     .setTitleResource(R.string.automatic_port)
@@ -524,55 +577,6 @@ public interface PreferencesHandler {
                     })
                     .build());
 
-            if(!isTV)
-            prefItems.add(PrefItem.newBuilder(context)
-                    .setIconResource(R.drawable.ic_prefs_storage_location)
-                    .setTitleResource(R.string.storage_location)
-                    .setPreferenceKey(Prefs.STORAGE_LOCATION)
-                    .hasNext(true)
-                    .setDefaultValue(StorageUtils.getIdealCacheDirectory(context))
-                    .setOnClickListener(new PrefItem.OnClickListener() {
-                        @Override
-                        public void onClick(final PrefItem item) {
-                            handler.openListSelection(item.getTitle(), null, SelectionMode.DIRECTORY, item.getValue(), 0, 0, new OnSelectionListener() {
-                                @Override
-                                public void onSelection(int position, Object value) {
-                                    if(value != null) {
-                                        item.saveValue(value);
-                                    } else {
-                                        item.clearValue();
-                                    }
-                                }
-                            });
-                        }
-                    })
-                    .setSubtitleGenerator(new PrefItem.SubtitleGenerator() {
-                        @Override
-                        public String get(PrefItem item) {
-                            return item.getValue().toString();
-                        }
-                    })
-                    .build());
-
-            prefItems.add(PrefItem.newBuilder(context)
-                    .setIconResource(R.drawable.ic_prefs_remove_cache)
-                    .setTitleResource(R.string.remove_cache)
-                    .setPreferenceKey(Prefs.REMOVE_CACHE)
-                    .setDefaultValue(true)
-                    .setOnClickListener(new PrefItem.OnClickListener() {
-                        @Override
-                        public void onClick(final PrefItem item) {
-                            item.saveValue(!(boolean) item.getValue());
-                        }
-                    })
-                    .setSubtitleGenerator(new PrefItem.SubtitleGenerator() {
-                        @Override
-                        public String get(PrefItem item) {
-                            boolean enabled = (boolean) item.getValue();
-                            return enabled ? context.getString(R.string.enabled) : context.getString(R.string.disabled);
-                        }
-                    })
-                    .build());
 
             prefItems.add(PrefItem.newBuilder(context).setTitleResource(R.string.advanced).build());
 
