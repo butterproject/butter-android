3ffddb961a71b33273e26f33268b2d25672f28cd
Blaž Šolar <solarblaz@gmail.com>
2018-06-18 11:25:46 +0200

    Play config fix

--- config/ci/download_play_config.sh
+++ config/ci/download_play_config.sh
@@ -2,7 +2,7 @@ if [[ $BUTTER_PLAY_CONFIG && ${BUTTER_PLAY_CONFIG} ]]
 then
     echo "Play config detected - downloading..."
     curl -L -o play_publish.json ${BUTTER_PLAY_CONFIG}
-    echo "playPublishFile=../play_publish.json" >> gradle.properties
+    echo "playPublishFile=play_publish.json" >> gradle.properties
 else
     echo "Play config uri not set. APK will not be uploaded."
 fi
